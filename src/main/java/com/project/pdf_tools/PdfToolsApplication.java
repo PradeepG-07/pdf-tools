fccfceed0f010de063298b3d4e0bca2f1d471b7d
Pradeep Golla <pradeepubuntuvps@gmail.com>
2026-06-28 22:29:52 +0530

    Init backend with project structure

---
+++ src/main/java/com/project/pdf_tools/PdfToolsApplication.java
@@ -0,0 +1,13 @@
+package com.project.pdf_tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfToolsApplication.class, args);
+	}
+
+}
